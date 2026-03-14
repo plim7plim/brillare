03e608a1fcccd7470b7e5f4bb829cb170b1e7f24
Plínio Peixoto Dos Santos <pliniobr33@gmail.com>
2026-03-14 19:16:10 -0300

    que de certo

--- script.js
+++ script.js
@@ -1,91 +1,85 @@
+function trocarBolsa(novaImagem){
+    document.getElementById("bolsa").src = novaImagem;
+}
+
 const botao = document.getElementById("modoNoturno");
 
 botao.addEventListener("click", () => {
-document.body.classList.toggle("dark");
+    document.body.classList.toggle("dark");
 });
 
 let carrinho = [];
 
 function toggleCarrinho(){
-const carrinhoBox = document.getElementById("carrinhoPreview");
-carrinhoBox.classList.toggle("ativo");
+    const carrinhoBox = document.getElementById("carrinhoPreview");
+    carrinhoBox.classList.toggle("ativo");
 }
 
 function mostrarNotificacao(texto){
+    const notificacao = document.getElementById("notificacao");
 
-const notificacao = document.getElementById("notificacao");
+    notificacao.innerText = texto;
+    notificacao.classList.add("mostrar");
 
-notificacao.innerText = texto;
-notificacao.classList.add("mostrar");
-
-setTimeout(()=>{
-notificacao.classList.remove("mostrar");
-},2500);
-
+    setTimeout(()=>{
+        notificacao.classList.remove("mostrar");
+    },2500);
 }
 
 function atualizarCarrinho(){
 
-const lista = document.getElementById("listaCarrinho");
-const contador = document.getElementById("contadorCarrinho");
-const total = document.getElementById("totalCarrinho");
+    const lista = document.getElementById("listaCarrinho");
+    const contador = document.getElementById("contadorCarrinho");
+    const total = document.getElementById("totalCarrinho");
 
-lista.innerHTML="";
+    lista.innerHTML="";
 
-let soma=0;
+    let soma=0;
 
-carrinho.forEach((produto,index)=>{
+    carrinho.forEach((produto,index)=>{
 
-soma+=produto.preco;
+        soma+=produto.preco;
 
-const item=document.createElement("div");
+        const item=document.createElement("div");
 
-item.innerHTML=`
-${produto.nome} - R$ ${produto.preco}
-<button onclick="removerItem(${index})">❌</button>
-`;
+        item.innerHTML=`
+        ${produto.nome} - R$ ${produto.preco}
+        <button onclick="removerItem(${index})">❌</button>
+        `;
 
-lista.appendChild(item);
+        lista.appendChild(item);
 
-});
+    });
 
-contador.innerText=carrinho.length;
-
-total.innerText="Total: R$ "+soma;
+    contador.innerText=carrinho.length;
+    total.innerText="Total: R$ "+soma;
 
 }
 
 function adicionarCarrinho(nome,preco){
-
-carrinho.push({nome,preco});
-
-atualizarCarrinho();
-
+    carrinho.push({nome,preco});
+    atualizarCarrinho();
 }
 
 function removerItem(index){
-
-carrinho.splice(index,1);
-
-atualizarCarrinho();
-
+    carrinho.splice(index,1);
+    atualizarCarrinho();
 }
 
 function finalizarCompra(){
 
-if(carrinho.length===0){
-alert("Carrinho vazio");
-return;
-}
+    if(carrinho.length===0){
+        alert("Carrinho vazio");
+        return;
+    }
 
-let mensagem="Olá! Gostaria de comprar:%0A";
+    let mensagem="Olá! Gostaria de comprar:%0A";
 
-carrinho.forEach(produto=>{
-mensagem+=`- ${produto.nome} R$ ${produto.preco}%0A`;
-});
+    carrinho.forEach(produto=>{
+        mensagem+=`- ${produto.nome} R$ ${produto.preco}%0A`;
+    });
 
-let url="https://wa.me/5562999230895?text="+mensagem;
+    let url="https://wa.me/5562999230895?text="+mensagem;
 
-window.open(url,"_blank");
-
+    window.open(url,"_blank");
 }
